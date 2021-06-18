f197a54092ed21b302857d749efabfb3b0dc4138
Akotet Y. Tesema <akiyeshaw@gmail.com>
2021-06-18 22:53:37 +0300

    I hate betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,15 +21,15 @@ divisor *= 10;
 }
 if (mod > 5)
 {
-printf("Last digit of %d is %d and is greater than 5 \n", n, mod);
+printf("Last digit of %d is %d and is greater than 5\n", n, mod);
 }
 else if (mod == 0)
 {
-printf("Last digit of %d is %d and is 0 \n", n, mod);
+printf("Last digit of %d is %d and is 0\n", n, mod);
 }
 else if (mod < 6 && mod != 0)
 {
-printf("Last digit of %d is %d and is less than 6 \n", n, mod);
+printf("Last digit of %d is %d and is less than 6\n", n, mod);
 }
 return (0);
 }
