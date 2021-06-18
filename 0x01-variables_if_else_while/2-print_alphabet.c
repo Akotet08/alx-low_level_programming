8c8710368753462cd77ccf9d265cd8352086a943
Akotet Y. Tesema <akiyeshaw@gmail.com>
2021-06-18 21:21:09 +0300

    quick fixes

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int main(void) 
+{
     int i = 0;
+
     while (i < 26){
         putchar('a' + i);
         i++;
     }
     putchar('\n');
+
+    return 0;
 }
