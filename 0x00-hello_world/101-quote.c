87b25ce006f7c14e43e0079c9b09f7f4759f9df4
Akotet Y. Tesema <akiyeshaw@gmail.com>
2021-06-19 17:41:03 +0300

    quick fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
  * main - prints quote
  *
  * Description - prints quote without printf
- * Return - returns 1
+ * Return: returns 1
  */
 
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58, 1, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
 	exit(EXIT_FAILURE);
 	return (1);
 }
