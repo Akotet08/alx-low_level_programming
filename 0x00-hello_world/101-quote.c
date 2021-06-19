996ddcc0df7997cee5f7d0a37e65b2282b352852
Akotet Y. Tesema <akiyeshaw@gmail.com>
2021-06-19 17:21:10 +0300

    c files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints quote
+ *
+ * Description - prints quote without printf
+ * Return - returns 1
+ */
+
+int main(void)
+{
+	put_Line(Standard_Error,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - implements printf
+ *
+ * Description - printf is used to print a sting
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
