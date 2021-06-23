fbfa532521377c4fc73380730d7444b8ebc81c8c
Akotet Y. Tesema <akiyeshaw@gmail.com>
2021-06-23 18:54:13 +0300

    sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - multiples of 3 and 5
+ *
+ * @n: number
+ *
+ * Description: multiples of 3 and 5
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i = 3, sum = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 ==0 )|| (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
