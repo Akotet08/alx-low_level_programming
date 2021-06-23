5a816c9ab1b0070986d856caed12243b5f7c4a04
Akotet Y. Tesema <akiyeshaw@gmail.com>
2021-06-23 16:08:25 +0300

    check _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - prints holberton
+ *
+ * Description: prints holberton
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	_putchar("Holberton");
+	
+}
