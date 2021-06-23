2c17f73684f6467bf88dbe7f15ba006ec8121cf6
Akotet Y. Tesema <akiyeshaw@gmail.com>
2021-06-23 18:44:55 +0300

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_to_98 - prints to 98
+ *
+ * @n: number
+ *
+ * Description: prints to 98
+ * Return: returns 0
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("\n");
+		return (0);
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		printf("\n");
+		return (0);
+	}
+}
