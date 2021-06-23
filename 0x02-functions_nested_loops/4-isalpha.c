bb3e6aa32d12c8bd0a2867e5d24b0a51822129ec
Akotet Y. Tesema <akiyeshaw@gmail.com>
2021-06-23 17:24:43 +0300

    is alphabetic

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _islower - determines if alphabetic
+ *
+ * @c: ascii value of char
+ *
+ * Description: determines if alphabetic
+ * Return: returns 1 for alphabetic; 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c > 96 && c < 123 || c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
