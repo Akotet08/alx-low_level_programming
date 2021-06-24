9b219533c10b27186be8418b618985ff2e8789af
Akotet Y. Tesema <akiyeshaw@gmail.com>
2021-06-24 19:01:38 +0300

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_diagonal - prints diagonal line
+ *
+ * @n: length of line
+ *
+ * Description: prints diagonal line
+ * Return: nothing
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0, j = 0;
+
+	while (i < n)
+	{
+		while (j < i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
