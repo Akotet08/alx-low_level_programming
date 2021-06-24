7c67646e8b004a2b47324080b6f3ca56a4e02c1f
Akotet Y. Tesema <akiyeshaw@gmail.com>
2021-06-24 19:28:37 +0300

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * main - Fizz-Buzz test
+ *
+ * Description: Fizz-Buzz test
+ * Return: 0
+ */
+
+int main(void)
+{
+	int  i = 1;
+
+	while (i < 101)
+	{
+		if ((i % 5 == 0) && (i % 3 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+			printf("%d ", i);
+		i++;
+
+	}
+	printf("\n");
+}
