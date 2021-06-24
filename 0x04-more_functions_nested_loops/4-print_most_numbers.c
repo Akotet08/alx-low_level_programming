25de6170d076be2cc51207d36b0bdecf34c20e01
Akotet Y. Tesema <akiyeshaw@gmail.com>
2021-06-24 18:00:03 +0300

    numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints numbers except for 2 and 4
+ *
+ * Description: prints numbers except for 2 and 4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		if (i == 2 || i == 4) continue;
+		_putchar(i);
+		i++;
+	}
+	_putchar('$' + '\n');
+	return (0);
+}
