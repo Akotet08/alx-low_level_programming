e0038d3be6932faa0aed06c249d4a15c97e44259
Akotet Y. Tesema <akiyeshaw@gmail.com>
2021-06-24 18:19:42 +0300

    numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,10 +12,9 @@ void print_most_numbers(void)
 
 	while (i < 58)
 	{
-		if (i == 50 || i == 52)
-			continue;
-		_putchar(i);
-		i++;
+		if (!(i == 50 || i == 52))
+			_putchar(i);
+			i++;
 	}
 	_putchar('\n');
 }
